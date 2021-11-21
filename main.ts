bb75eadc7fda81a6d0a9cf9e33a508e2e36bc7b6
Yongsit Meeprasert <ginosty@outlook.com>
2021-11-21 18:11:02 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@ enum PingUnit {
  * Sonar and ping utilities
  */
 //% color="#2c3e50" weight=10
-namespace sonar {
+namespace CalibratableSonar {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
      * @param trig tigger pin
